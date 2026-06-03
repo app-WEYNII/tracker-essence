fb3b5d865b21bfcafb9f521442745d0b4104e75f
app-WEYNII <lucadjouadii@gmail.com>
2026-06-03 18:05:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    By WEYNII
    ============================================================ */
 
-const CACHE_NAME = 'monplein-v6';
+const CACHE_NAME = 'monplein-v7';
 
 // Fichiers à mettre en cache lors de l'installation
 const ASSETS_TO_CACHE = [
